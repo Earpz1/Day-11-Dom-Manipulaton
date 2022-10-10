f570a9098b822c87da1978a4aefe5cb6c46bcd35
Daniel <dan.earp@icloud.com>
2022-10-10 14:27:20 +0100

    Initial Upload

---
+++ js.js
@@ -0,0 +1,38 @@
+function changeTitle() {
+  document.getElementById('page-title').innerHTML = 'Negozio di smartphone'
+}
+
+function changeBackground() {
+  document.body.style.backgroundColor = 'red'
+}
+
+function toggleFooter() {
+  document.getElementById('address').innerHTML =
+    'My fake address is in the North Pole'
+}
+
+function largeLinks() {
+  const links = document.querySelectorAll('a')
+
+  for (let i = 0; i < links.length; i++) {
+    links[i].classList.toggle('accessed')
+  }
+}
+
+function showImage() {
+  const images = document.getElementsByClassName('phone-image')
+
+  for (let i = 0; i < images.length; i++) {
+    images[i].classList.toggle('hideImage')
+  }
+}
+
+function priceColor() {
+  let colors = ['red', 'green', 'blue', 'orange', 'yellow', 'black', 'purple']
+  const price = document.getElementsByClassName('price')
+  const random = Math.floor(Math.random() * colors.length)
+
+  for (let i = 0; i < price.length; i++) {
+    price[i].style.color = colors[random]
+  }
+}
